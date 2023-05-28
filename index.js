7154fc99dede81203c41ab41b4329e41f57fb285
jeevanandham5 <jeevajeevanandham30@gmail.com>
2023-05-28 11:09:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>iam working :-)</h1>");
+});
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected:${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User With Id:${socket.id} Joined Room:${data}`);
+  });
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_massage", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnect", socket.id);
+  });
+});
+
+// starting the server in port:3001
+server.listen(process.env.PORT || 3001, () => {
+  console.log("SERVER IS RUNNING");
+});
